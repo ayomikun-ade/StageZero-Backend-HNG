9c73a080a4621f07784c6f48beeadd33d261313f
Ayomikun <126820097+ayomikun-ade@users.noreply.github.com>
2025-02-02 10:46:52 +0100

    api response edit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json({
+  res.status(200).json({
     email: "ayoadeosun10@gmail.com",
     current_datetime: new Date().toISOString(),
     github_url: "https://github.com/ayomikun-ade/StageZero-Backend-HNG",
